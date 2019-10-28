11d64f6065b72681ecf0105a96d99e2a242c204c
olesyamogil <olesya.mogil@gmail.com>
2019-10-28 17:12:41 +0200

    Updates

---
+++ precache-manifest.29d5939edea69aeb78f27f9043fa5d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258db209b60ee048775e8ac9acc60c83",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "e32f9aa784e5561c58aa",
+    "url": "/react_static-list-of-posts/static/css/main.26141018.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_static-list-of-posts/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "e32f9aa784e5561c58aa",
+    "url": "/react_static-list-of-posts/static/js/main.a983d412.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
